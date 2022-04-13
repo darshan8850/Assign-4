26177f807b55f31ac07eb5764a7ca78ce0887058
darshan8850 <darshanpatil05092001@gmail.com>
2022-04-14 04:53:45 +0530

    done

---
+++ model.py
@@ -0,0 +1,84 @@
+# baseline model for the dogs vs cats dataset
+import sys
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+ 
+# define cnn model
+def define_model():
+  model = Sequential()
+  model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+  model.add(MaxPooling2D((2, 2)))
+  model.add(Flatten())
+  model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+  model.add(Dense(1, activation='sigmoid'))
+  # compile model
+  opt = SGD(lr=0.001, momentum=0.9)
+  model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+  return model
+ 
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+  # plot loss
+  pyplot.subplot(211)
+  pyplot.title('Cross Entropy Loss')
+  pyplot.plot(history.history['loss'], color='blue', label='train')
+  pyplot.plot(history.history['val_loss'], color='orange', label='test')
+  # plot accuracy
+  pyplot.subplot(212)
+  pyplot.title('Classification Accuracy')
+  pyplot.plot(history.history['accuracy'], color='blue', label='train')
+  pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+  # save plot to file
+  filename = sys.argv[0].split('/')[-1]
+  pyplot.savefig(filename + '_plot.png')
+  pyplot.close()
+
+def plotmodelhistory(history): 
+    fig, axs = plt.subplots(1,2,figsize=(15,5)) 
+    # summarize history for accuracy
+    axs[0].plot(history.history['accuracy']) 
+    axs[0].plot(history.history['val_accuracy']) 
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy') 
+    axs[0].set_xlabel('Epoch')
+    
+    axs[0].legend(['train', 'validate'], loc='upper left')
+    # summarize history for loss
+    axs[1].plot(history.history['loss']) 
+    axs[1].plot(history.history['val_loss']) 
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss') 
+    axs[1].set_xlabel('Epoch')
+    axs[1].legend(['train', 'validate'], loc='upper left')
+    plt.show()
+
+# run the test harness for evaluating a model
+def run_test_harness():
+  # define model
+  model = define_model()
+  # create data generator
+  datagen = ImageDataGenerator(rescale=1.0/255.0)
+  # prepare iterators
+  train_it = datagen.flow_from_directory('dataset_aero_vs_dirt/train/',
+  class_mode='binary', batch_size=64, target_size=(200, 200))
+  test_it = datagen.flow_from_directory('dataset_aero_vs_dirt/test/',
+  class_mode='binary', batch_size=64, target_size=(200, 200))
+  # fit model
+  history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+  validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
+  # evaluate model
+  _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+  print('> %.3f' % (acc * 100.0))
+  # learning curves
+  summarize_diagnostics(history)
+  plotmodelhistory(history)
+ 
+# entry point, run the test harness
+run_test_harness()
